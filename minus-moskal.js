576873c9d54e6563d33dba0e6d802e17e70715ab
Constantine Ketskalo <constantine.ketskalo@gmail.com>
2025-04-28 15:17:59 +0300

    Згенерований код нового скрипта розширенням tempermonkey.

---
+++ minus-moskal.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-28
+// @description  try to take over the world!
+// @author       You
+// @match        https://bitbucket.org/const_k/minus-moskal/src/main/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bitbucket.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
